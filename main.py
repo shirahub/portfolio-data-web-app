24bf364e1979484d4a5a6a5b66e41e152fb0ab3a
Shirleen <shirleen.adriana@gmail.com>
2024-01-20 17:31:53 +0700

    init main

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from src.resume import show_resume
+
+st.set_page_config(
+    page_title="Shirleen Data Web App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# Shirleen Web App"
+    }
+)
+
+with st.sidebar:
+    st.title("Resume")
+    st.title("Research")
+
+show_resume()
